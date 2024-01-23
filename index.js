95a574890e723d0d47c10c852a08da27fe9fc791
@Infer <96887546+Infernite@users.noreply.github.com>
2024-01-23 07:09:56 +0630

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const { Client } = require('discord.js');
+// Import the discord.js library with intents
+const { Client, Intents } = require('discord.js');
 
-// Create a new Discord client
-const client = new Client();
+// Create a new Discord client with intents
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 // Set the bot's status to invisible
 client.once('ready', () => {
-client.user.setPresence({ status: 'invisible' });
-console.log('Bot is ready!');
+  client.user.setPresence({ status: 'invisible' });
+  console.log('Bot is ready!');
 });
+
+// Login to Discord with your bot token
 client.login(process.env.token);

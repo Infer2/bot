fbe68c76d60472864ca39ba0c879ac9d532b6ac3
@Infer <96887546+Infernite@users.noreply.github.com>
2024-01-23 07:39:15 +0630

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-// Import the discord.js library
-const { Client, GatewayIntentBits } = require('discord.js');
-
-// Create a new Discord client
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
-});
-
-// Set the bot's status to invisible
-client.once('ready', () => {
-  client.user.setPresence({ status: 'invisible' });
-  console.log('Bot is ready!');
-});
-
-// Login to Discord with your bot token
-client.login(process.env.token);
+const express = require("express"),
+	{
+		Client: Client,
+		GatewayIntentBits: GatewayIntentBits
+	} = require("discord.js"),
+	app = express(),
+	client = new Client({
+		intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+	});
+client.on("ready", (async () => {
+	console.log("ready")
+})), app.get("/ping", ((e, t) => {
+	t.send(new Date)
+}));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, (() => {
+	console.log(`Listening to ${PORT}`)
+})), client.login(process.env.token);

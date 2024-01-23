965c508066cb1e57af16943e43ebcddfb2de03d4
@Infer <96887546+Infernite@users.noreply.github.com>
2024-01-23 07:50:24 +0630

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const express = require("express"),
-	{
-		Client: Client,
-		GatewayIntentBits: GatewayIntentBits
-	} = require("discord.js"),
-	app = express(),
-	client = new Client({
-		intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
-	});
-client.on("ready", (async () => {
-	console.log("ready")
-})), app.get("/ping", ((e, t) => {
-	t.send(new Date)
-}));
+const express = require('express');
+const { Client, Intents } = require('discord.js');
+const app = express();
+
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.DIRECT_MESSAGES,
+  ],
+});
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+app.get('/ping', (req, res) => {
+  res.send(new Date);
+});
+
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, (() => {
-	console.log(`Listening to ${PORT}`)
-})), client.login(process.env.token);
+app.listen(PORT, () => {
+  console.log(`Listening to ${PORT}`);
+});
+
+client.login(process.env.token);

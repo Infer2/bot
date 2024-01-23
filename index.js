511bb9c6ad94fe6ed98ab5e74181572d56266b4e
@Infer <96887546+Infernite@users.noreply.github.com>
2024-01-23 07:04:00 +0630

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const {
-	Client: Client,
-	GatewayIntentBits: GatewayIntentBits
-} = require("discord.js"), client = new Client({
-	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+const { Client } = require('discord.js');
+
+// Create a new Discord client
+const client = new Client();
+
+// Set the bot's status to invisible
+client.once('ready', () => {
+client.user.setPresence({ status: 'invisible' });
+console.log('Bot is ready!');
 });
-client.once("ready", (() => {
-	client.user.setStatus("invisible"), console.log("Bot is ready!")
-})), client.login(process.env.token);
+client.login(process.env.token);

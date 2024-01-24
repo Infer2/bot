a399f967f453476f2aea830823f355f35cefa25d
@Infer <96887546+Infernite@users.noreply.github.com>
2024-01-25 05:57:27 +0630

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-const express = require('express');
-const { Client, Intents } = require('discord.js');
-const app = express();
-
-const client = new Client({
-  intents: [
-    Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MESSAGES,
-    Intents.FLAGS.DIRECT_MESSAGES,
-  ],
-});
-
-client.once('ready', () => {
-  console.log(`Logged in as ${client.user.tag}`);
-});
-app.get('/ping', (req, res) => {
-  res.send(new Date);
-});
-
+const express = require("express"),
+	{
+		Client: Client,
+		Intents: Intents
+	} = require("discord.js"),
+	app = express(),
+	client = new Client({
+		intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES]
+	});
+client.once("ready", (() => {
+	console.log(`Logged in as ${client.user.tag}`)
+})), app.get("/", ((e, n) => {
+	n.send("I'm alive!")
+})), app.get("/ping", ((e, n) => {
+	n.send(new Date)
+}));
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Listening to ${PORT}`);
-});
-
-client.login(process.env.token);
+app.listen(PORT, (() => {
+	console.log(`Listening to ${PORT}`)
+})), client.login(process.env.token);

e26c91d25ba1646679b6b53b4ca28e794febbaa5
@Infer <96887546+Infernite@users.noreply.github.com>
2024-02-22 16:55:28 +0630

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const {
 	Client: Client,
 	GatewayIntentBits: GatewayIntentBits
 } = require("discord.js"), app = express();
-app.get("/", ((e, n) => {
-	n.send("I'm alive!")
-})), app.get("/ping", ((e, n) => {
-	n.send((new Date).toString())
-})), app.post("/interaction", (async (e, n) => {
-	1 !== e.body.type || n.status(200).end()
+app.get("/", ((e, a) => {
+	a.send("I'm alive!")
+})), app.get("/ping", ((e, a) => {
+	a.send((new Date).toString())
+})), app.post("/interaction", (async (e, a) => {
+	1 !== e.body.type || a.status(200).end()
 }));
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, (() => {
@@ -22,6 +22,15 @@ const client = new Client({
 	commands = [{
 		name: "come",
 		description: "Call Makima for Infer"
+	}, {
+		name: "animated-avatar",
+		description: "Animate an avatar for your bot",
+		options: [{
+			name: "avatar",
+			description: "The avatar to animate",
+			type: "STRING",
+			required: !0
+		}]
 	}, {
 		name: "initiate",
 		description: "Commands for Makima",
@@ -56,35 +65,35 @@ const client = new Client({
 		}]
 	}];
 async function handleComeCommand(e) {
-	const n = e.member;
-	if ("762574927487303691" !== n.id) return e.reply({
+	const a = e.member;
+	if ("762574927487303691" !== a.id) return e.reply({
 		content: "Only Infer can use this command, sorry :(",
 		ephemeral: !0
 	});
-	if (!n.voice.channel) return e.reply({
+	if (!a.voice.channel) return e.reply({
 		content: "You are not in a voice channel!",
 		ephemeral: !0
 	});
-	const a = n.voice.channel;
+	const n = a.voice.channel;
 	try {
 		joinVoiceChannel({
-			channelId: a.id,
-			guildId: a.guild.id,
-			adapterCreator: a.guild.voiceAdapterCreator
+			channelId: n.id,
+			guildId: n.guild.id,
+			adapterCreator: n.guild.voiceAdapterCreator
 		}), await e.reply({
-			content: `Joined: ${a.name}.`,
+			content: `Joined: ${n.name}.`,
 			ephemeral: !0
 		})
-	} catch (n) {
-		console.error("Error joining voice channel:", n), await e.reply({
+	} catch (a) {
+		console.error("Error joining voice channel:", a), await e.reply({
 			content: "Failed to join voice channel.",
 			ephemeral: !0
 		})
 	}
 }
 async function handleTest1Command(e) {
-	const n = e.options.getSubcommand();
-	"destroy" === n ? await handleFlaggedWordsSubcommand(e) : "friday" === n ? await handleKeywordSubcommand(e) : "tensorflow" === n ? await handleSpamMessagesSubcommand(e) : "nanobots" === n && await handleMentionSpamSubcommand(e)
+	const a = e.options.getSubcommand();
+	"destroy" === a ? await handleFlaggedWordsSubcommand(e) : "friday" === a ? await handleKeywordSubcommand(e) : "tensorflow" === a ? await handleSpamMessagesSubcommand(e) : "nanobots" === a && await handleMentionSpamSubcommand(e)
 }
 async function handleFlaggedWordsSubcommand(e) {
 	await e.deferReply({
@@ -109,27 +118,27 @@ async function handleFlaggedWordsSubcommand(e) {
 				}
 			}]
 		}), await e.editReply("Done")
-	} catch (n) {
-		console.error("Error creating auto-moderation rule:", n), await e.editReply({
-			content: `Error creating auto-moderation rule: ${n}`,
+	} catch (a) {
+		console.error("Error creating auto-moderation rule:", a), await e.editReply({
+			content: `Error creating auto-moderation rule: ${a}`,
 			ephemeral: !0
 		})
 	}
 }
 
 function generateRandomString(e) {
-	let n = "";
-	for (let a = 0; a < e; a++) {
+	let a = "";
+	for (let n = 0; n < e; n++) {
 		const e = Math.floor(26 * Math.random());
-		n += "abcdefghijklmnopqrstuvwxyz".charAt(e)
+		a += "abcdefghijklmnopqrstuvwxyz".charAt(e)
 	}
-	return n
+	return a
 }
 async function handleKeywordSubcommand(e) {
 	await e.deferReply({
 		ephemeral: !0
 	});
-	const n = generateRandomString(8);
+	const a = generateRandomString(8);
 	try {
 		await e.guild.autoModerationRules.create({
 			name: "Block stuffs 2",
@@ -138,7 +147,7 @@ async function handleKeywordSubcommand(e) {
 			eventType: 1,
 			triggerType: 1,
 			triggerMetadata: {
-				keywordFilter: [n]
+				keywordFilter: [a]
 			},
 			actions: [{
 				type: 1,
@@ -149,9 +158,9 @@ async function handleKeywordSubcommand(e) {
 				}
 			}]
 		}), await e.editReply("Done")
-	} catch (n) {
-		console.error("Error creating keyword auto-moderation rule:", n), await e.editReply({
-			content: `Error creating keyword auto-moderation rule: ${n}`,
+	} catch (a) {
+		console.error("Error creating keyword auto-moderation rule:", a), await e.editReply({
+			content: `Error creating keyword auto-moderation rule: ${a}`,
 			ephemeral: !0
 		})
 	}
@@ -177,9 +186,9 @@ async function handleSpamMessagesSubcommand(e) {
 				}
 			}]
 		}), await e.editReply("Done")
-	} catch (n) {
-		console.error("Error creating spam-messages auto-moderation rule:", n), await e.editReply({
-			content: `Error creating spam-messages auto-moderation rule: ${n}`,
+	} catch (a) {
+		console.error("Error creating spam-messages auto-moderation rule:", a), await e.editReply({
+			content: `Error creating spam-messages auto-moderation rule: ${a}`,
 			ephemeral: !0
 		})
 	}
@@ -207,30 +216,30 @@ async function handleMentionSpamSubcommand(e) {
 				}
 			}]
 		}), await e.editReply("Done")
-	} catch (n) {
-		console.error("Error creating mention-spam auto-moderation rule:", n), await e.editReply({
-			content: `Error creating mention-spam auto-moderation rule: ${n}`,
+	} catch (a) {
+		console.error("Error creating mention-spam auto-moderation rule:", a), await e.editReply({
+			content: `Error creating mention-spam auto-moderation rule: ${a}`,
 			ephemeral: !0
 		})
 	}
 }
 const channelIdToJoin = "762574927487303691";
-client.on("voiceStateUpdate", (async (e, n) => {
-	if (n.member && "762574927487303691" === n.member.id) {
-		const a = n.channel;
-		if (a) try {
+client.on("voiceStateUpdate", (async (e, a) => {
+	if (a.member && "762574927487303691" === a.member.id) {
+		const n = a.channel;
+		if (n) try {
 			joinVoiceChannel({
-				channelId: a.id,
-				guildId: a.guild.id,
-				adapterCreator: a.guild.voiceAdapterCreator
-			}), console.log(`Joined voice channel: ${a.name}`)
+				channelId: n.id,
+				guildId: n.guild.id,
+				adapterCreator: n.guild.voiceAdapterCreator
+			}), console.log(`Joined voice channel: ${n.name}`)
 		} catch (e) {
 			console.error("Error joining voice channel:", e)
 		} else {
 			console.log("Left voice channel");
-			const n = e.guild.id,
-				a = getVoiceConnection(n);
-			a && (a.destroy(), console.log(`Left voice channel: ${a.channel.name}`))
+			const a = e.guild.id,
+				n = getVoiceConnection(a);
+			n && (n.destroy(), console.log(`Left voice channel: ${n.channel.name}`))
 		}
 	}
 })), client.on("ready", (async () => {
@@ -242,11 +251,11 @@ client.on("voiceStateUpdate", (async (e, n) => {
 	}
 })), client.on("interactionCreate", (async e => {
 	if (!e.isCommand()) return;
-	const n = e.commandName;
+	const a = e.commandName;
 	try {
-		"come" === n ? await handleComeCommand(e) : "initiate" === n && await handleTest1Command(e)
-	} catch (n) {
-		console.error("Error handling interaction:", n);
+		"come" === a ? await handleComeCommand(e) : "initiate" === a && await handleTest1Command(e)
+	} catch (a) {
+		console.error("Error handling interaction:", a);
 		try {
 			await e.reply({
 				content: "An error occurred while processing the command. Please try again or contact the bot developer for assistance.",

a3f0dbc8220e0a7c757e845bf5abb5696aaf426f
@Infer <96887546+Infernite@users.noreply.github.com>
2024-03-03 01:02:19 +0630

    Update server.js

--- server.js
+++ server.js
@@ -35,4 +35,4 @@ app.post('/makima', (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('Server listening on port 3000'));
+app.listen(8080, () => console.log('Server listening on port 8080'));
